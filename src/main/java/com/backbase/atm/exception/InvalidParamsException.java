e4c9f0e7ccb88563e39953adf3b03fca9c61c15e
Vj Gajjela <vijaygajjelli@gmail.com>
2017-05-15 23:21:00 +0530

    Invalid parameters exception

---
+++ src/main/java/com/backbase/atm/exception/InvalidParamsException.java
@@ -0,0 +1,13 @@
+package com.backbase.atm.exception;
+
+public class InvalidParamsException extends Exception {
+
+	/**
+	 * serial version id
+	 */
+	private static final long serialVersionUID = -8276311311566257142L;
+
+	public InvalidParamsException(final String message) {
+		super(message);
+	}
+}
